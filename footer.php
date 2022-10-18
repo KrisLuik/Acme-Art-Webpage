92c9cb9f543eb418ce8e9db5c021c421ca3cb928
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-10-18 09:29:54 +0800

    Change html to php

---
+++ footer.php
@@ -0,0 +1,9 @@
+    <!--FOOTER START -->
+    <footer>
+        <div class="row">
+            <div class="col-md-6 ms-2">
+                <p>Copyright &copy; Kristiin Tribbeck </p>
+            </div>
+        </div>
+    </footer>
+    <!--FOOTER END -->
