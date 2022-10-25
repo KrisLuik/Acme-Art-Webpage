6d68ae4b359db3131de32c5891cb06f259bd67c2
Kris <kristribbeck@gmail.com>
2022-10-25 15:08:41 +0800

    Search Bar functionality

--- searchPaintings.php
+++ searchPaintings.php
@@ -4,28 +4,27 @@
      ID: 30045325
      Description: creation of a multi-page client-server website for a local art gallery called Acme Arts.
 -->
+
 <head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<!-- Optimize code for mobile devices first and then scale up components as necessary using CSS media queries. -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Search | Assessment Three</title>
-<!-- Linking HTML to CSS -->
-<link rel="stylesheet" href="assessment_3.css">
-<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <!-- Optimize code for mobile devices first and then scale up components as necessary using CSS media queries. -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>All Paintings | Assessment Three</title>
+    <!-- Linking HTML to CSS -->
+    <link rel="stylesheet" href="assessment_3.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <!-- Grey with black text-->
     <?php
-	  include_once('inc_nav.php');
-      // test below file to check if footer works. 
-      // include_once('footer.php');
+    include_once('inc_nav.php');
+    // test below file to check if footer works. 
+    // include_once('footer.php');
     ?>
     <!--Comment out the nav bar later and change the extension to .php when using PHP. Start from here.-->
     <!--  <nav class="navbar navbar-dark bg-dark fixed-top">-->
@@ -123,69 +122,67 @@
         </div>
     </nav> -->
     <!--Comment out the nav bar later and change the extension to .php when using PHP. End here.-->
-  <!-- START PHP -->
- <?php 
- session_start();
-    if(isset($_SESSION['message'])){
-        ?>
-     <div class="alert alert-info text-center" style="margin-top:20px;">
-         <?php echo $_SESSION['message']; ?>
-     </div>
-     <?php
+    <!-- START PHP -->
+    <?php
+    session_start();
+    if (isset($_SESSION['message'])) {
+    ?>
+        <div class="alert alert-info text-center" style="margin-top:20px;">
+            <?php echo $_SESSION['message']; ?>
+        </div>
+    <?php
 
-     unset($_SESSION['message']);
- }
-?>
-<!-- FINISH PHP-->
+        unset($_SESSION['message']);
+    }
+    ?>
+    <!-- FINISH PHP-->
 
-  <!--Start of table.-->
-  <div class="container-fluid" id="containerStyle">
-    <table class="table table-hover" style="margin-top:20px;">
-      <thead class="table-secondary">
-        <th>Title</th>
-        <th>Year</th>
-        <th>Painting</th>
-        <th>Artist</th>
-        <th>Style</th>
-      </thead>
-      <tbody>
-        <!--PHP START-->
-     <?php
-         //include our connection
-         include_once('connection.php');
-
-         $database = new Connection();
-         $db = $database->open();
-         $query = $_POST['query'];
-         try{	
-             $sql = "SELECT * FROM Painting_Data WHERE Title LIKE '$query%'";
-             $result = $db->query($sql);
-             if($result->rowCount() > 0) {
-                ?>
-                 <tr>
-                     <td><?php echo $row['Title']; ?></td>
-                     <td><?php echo $row['Year_Painted']; ?></td>
-                     <td><?php echo '<img src="data:image/png;base64,'.base64_encode($row['Painting']).'"/>'; ?></td>
-                     <td><?php echo $row['Artist']; ?></td>
-                     <td><?php echo $row['Style']; ?></td>
-                 </tr>
-                 <?php 
-             }
-             else {
-                 echo "No results found";
-             }
-         }
-         catch(PDOException $e){
-             echo "There is some problem in connection: " . $e->getMessage();
-         }
-         //close connection
-         $database->close();
-     ?>
-    <!--FINISH PHP-->
-      </tbody>
-    </table>
-  </div>
-  <!-- FOOTER START -->
+    <!--Start of table.-->
+    <div class="container-fluid" id="containerStyle">
+        <!--Testing Search bar Start -->
+        <form class="d-flex mt-3" role="search" action="all-paintings.php" method="GET">
+            <input class="form-control me-2" type="text" name="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-success" type="submit">Search</button>
+        </form>
+        <!--Search Bar Finish -->
+        <table class="table table-hover" style="margin-top:20px;">
+            <thead class="table-secondary">
+                <th>Title</th>
+                <th>Year</th>
+                <th>Artist</th>
+                <th>Style</th>
+                <th>Thumbnail</th>
+            </thead>
+            <?php
+            $search = $_GET['search'];
+            include_once('connection.php');
+            $database = new Connection();
+            $db = $database->open();
+            try {
+                $sql = "SELECT Title, Year_Painted, Artist, Style, Painting FROM Painting_Data WHERE Title LIKE '%$search%'";
+                $row = $db->query($sql);
+                foreach ($db->query($sql) as $row) {
+            ?>
+                    <tr>
+                        <td><?php echo $row['Title']; ?></td>
+                        <td><?php echo $row['Year_Painted']; ?></td>
+                        <td><?php echo $row['Artist']; ?></td>
+                        <td><?php echo $row['Style']; ?></td>
+                        <td><?php echo '<img class="thumb" src="data:image/png;base64,' . base64_encode($row['Painting']) . '"/>'; ?></td>
+                    </tr>
+            <?php
+                }
+            } catch (PDOException $e) {
+                echo "There is some problem in connection: " . $e->getMessage();
+            }
+            //close connection
+            $database->close();
+            ?>
+            <!--FINISH PHP-->
+            </tbody>
+        </table>
+    </div>
+    <!-- FOOTER START -->
     <!-- <footer>
         <div class="row">
             <div class="col-md-6 ms-2">
