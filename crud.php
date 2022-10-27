17c2ec0d6d67afd3ecfb71d052381bcc5972831f
KrisTribbeck <kristribbeck@gmail.com>
2022-10-27 11:18:46 +0800

    Update crud.php

--- crud.php
+++ crud.php
@@ -2,14 +2,15 @@
 <html lang="en">
 <!-- Name: Kristiin Tribbeck
      ID: 30045325
-     Description: creation of a multi-page client-server website for a local art gallery called Acme Arts.-->
+     Description: creation of a multi-page client-server website for a local art gallery called Acme Arts.
+-->
 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <!-- Optimize code for mobile devices first and then scale up components as necessary using CSS media queries. -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home | Assessment Three</title>
+    <title>All Paintings | Assessment Three</title>
     <!-- Linking HTML to CSS -->
     <link rel="stylesheet" href="assessment_3.css">
     <!-- Bootstrap CSS -->
@@ -19,156 +20,81 @@
 </head>
 
 <body>
-    <!-- Grey with black text -->
+    <!-- Grey with black text-->
     <?php
     include_once('inc_nav.php');
-    // test below file to check if footer works. 
-    // include_once('footer.php');
     ?>
-    <!--Comment out the nav bar later and change the extension to .php when using PHP. Start from here.-->
-    <!--  <nav class="navbar navbar-dark bg-dark fixed-top">-->
-    <!-- <nav class="navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Acme Art Gallery</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
-                aria-labelledby="offcanvasDarkNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Painting Collection</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
-                        aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <div>
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">All Paintings</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">All Artists</a>
-                                <ul class="dropdown-menu dropdown-menu-dark">
-                                    <li><a class="dropdown-item" href="artist1.php">August Renoir</a></li>
-                                    <li><a class="dropdown-item" href="artist2.php">Michelangelo</a></li>
-                                    <li><a class="dropdown-item" href="artist3.php">Vincent Van Gogh</a></li>
-                                    <li><a class="dropdown-item" href="artist4.php">Leonardo Da Vinci</a></li>
-                                    <li><a class="dropdown-item" href="artist5.php">Claude Monet</a></li>
-                                    <li><a class="dropdown-item" href="artist6.php">Pablo Picasso</a></li>
-                                    <li><a class="dropdown-item" href="artist7.php">Salvador Dali</a></li>
-                                    <li><a class="dropdown-item" href="artist8.php">Paul Cezanne</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">Paintings by Style</a>
-                                <ul class="dropdown-menu dropdown-menu-dark">
-                                    <li><a class="dropdown-item" href="#">Impressionism</a></li>
-                                    <li><a class="dropdown-item" href="#">Mannerism</a></li>
-                                    <li><a class="dropdown-item" href="#">Still-life</a></li>
-                                    <li><a class="dropdown-item" href="#">Portrait</a></li>
-                                    <li><a class="dropdown-item" href="#">Realism</a></li>
-                                    <li><a class="dropdown-item" href="#">Cubism</a></li>
-                                    <li><a class="dropdown-item" href="#">Surrealism</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">Paintings by Name</a>
-                                <ul class="dropdown-menu dropdown-menu-dark">
-                                    <li><a class="dropdown-item" href="#"></a></li>
-                                    <li><a class="dropdown-item" href="#">At the Lapin Agile</a></li>
-                                    <li><a class="dropdown-item" href="#">Bal du moulin de la Galette</a></li>
-                                    <li><a class="dropdown-item" href="#">Cafe Terrace at Night</a></li>
-                                    <li><a class="dropdown-item" href="#">Doni Tondo (Doni Madonna)</a></li>
-                                    <li><a class="dropdown-item" href="#">Houses of Parliament</a></li>
-                                    <li><a class="dropdown-item" href="#">Jaz de Bouffan</a></li>
-                                    <li><a class="dropdown-item" href="#">Mona Lisa</a></li>
-                                    <li><a class="dropdown-item" href="#">Nature morte au compotier</a></li>
-                                    <li><a class="dropdown-item" href="#">Sunrise</a></li>
-                                    <li><a class="dropdown-item" href="#">The Hallucinogenic Toreador</a></li>
-                                    <li><a class="dropdown-item" href="#">The Persistence of Memory</a></li>
-                                    <li><a class="dropdown-item" href="#">The Kingfisher</a></li>
-                                    <li><a class="dropdown-item" href="#">The Potato Eaters</a></li>
-                                    <li><a class="dropdown-item" href="#">Vase with Twelve Sunflowers</a></li>
-                                    <li><a class="dropdown-item" href="#">Vitruvian Man</a></li>
-                                    <li><a class="dropdown-item" href="#">Weaver</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Search Bar here?</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <form class="d-flex mt-3" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
+    <!-- START PHP -->
+    <?php
+    session_start();
+    if (isset($_SESSION['message'])) {
+    ?>
+        <div class="alert alert-info text-center" style="margin-top:20px;">
+            <?php echo $_SESSION['message']; ?>
         </div>
-    </nav> -->
-    <!--Comment out the nav bar later and change the extension to .php when using PHP. End here.-->
-    <!--MAIN BODY START (Make it into a separate file as well (?) i.e., like the navbar)-->
-    <div class="container-fluid" id="containerStyle">
-        <div class="p-3 my-3 border border-info rounded">
-            <h2>Update Painting Database</h2>
-            <form>
-                    <div class="form-group">
-                        <label for="InputTitle">Title</label>
-                        <input type="text" class="form-control" id="InputTitle" aria-describedby="emailHelp" placeholder="Enter the painting name:">
-                        <br>
-                    </div>
-                    <div class="form-group">
-                        <label for="InputPainting">Painting</label>
-                        <input type="file" class="form-control" id="InputPainting">
-                        <br>
-                    </div>
-                    <div class="form-group">
-                        <label for="InputYearPainted">Year Painted</label>
-                        <input type="number" class="form-control" id="InputYearPainted" placeholder="Enter the year the painting was painted:">
-                        <br>
-                    </div>
-                    <div class="form-group">
-                        <label for="InputMedia">Media Type</label>
-                        <input type="text" class="form-control" id="InputMedia" placeholder="Enter the media type of the painting:">
-                        <br>
-                    </div>
-                    <div class="form-group">
-                        <label for="InputArtist">Artist Name</label>
-                        <input type="text" class="form-control" id="InputArtist" placeholder="Enter the artist name of the painting:">
-                        <br>
-                    </div>
-                    <div class="form-group">
-                        <label for="InputStyle">Painting Style</label>
-                        <input type="text" class="form-control" id="InputStyle" placeholder="Enter the style of the painting:">
-                        <br>
-                    </div>
-                    <div>
-                    <button type="create" class="btn btn-primary">Add</button>
-                    <button type="submit" class="btn btn-primary">Edit</button>
-                    <button type="delete" class="btn btn-primary">Delete</button>
-                    <br>
-                    </div>
+    <?php
 
-                </form>
-        </div>
+        unset($_SESSION['message']);
+    }
+    ?>
+    <!-- FINISH PHP-->
+
+    <!--Start of table.-->
+    <div class="container-fluid" id="containerStyle">
+        <!--Testing Search bar Start -->
+        <form class="d-flex mt-3" role="search" action="all-paintings.php" method="GET">
+            <input class="form-control me-2" type="text" name="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-success" type="submit">Search</button>
+        </form>
+        <!--Search Bar Finish -->
+        <table class="table table-hover" style="margin-top:20px;">
+            <thead class="table-secondary">
+                <th>Title</th>
+                <th>Year</th>
+                <th>Artist</th>
+                <th>Style</th>
+                <th>Thumbnail</th>
+            </thead>
+            <?php
+            $search = $_GET['search'];
+            include_once('connection.php');
+            $database = new Connection();
+            $db = $database->open();
+            try {
+                $sql = "SELECT Title, Year_Painted, Artist, Style, Painting FROM Painting_Data WHERE Title LIKE '%$search%'";
+                $row = $db->query($sql);
+                foreach ($db->query($sql) as $row) {
+            ?>
+                    <tr>
+                        <td><?php echo $row['Title']; ?></td>
+                        <td><?php echo $row['Year_Painted']; ?></td>
+                        <td><?php echo $row['Artist']; ?></td>
+                        <td><?php echo $row['Style']; ?></td>
+                        <td><?php echo '<img class="thumb" src="data:image/png;base64,' . base64_encode($row['Painting']) . '"/>'; ?></td>
+                    </tr>
+            <?php
+                }
+            } catch (PDOException $e) {
+                echo "There is some problem in connection: " . $e->getMessage();
+            }
+            //close connection
+            $database->close();
+            ?>
+            <!--FINISH PHP-->
+            </tbody>
+        </table>
     </div>
-    <!--MAIN BODY END -->
-    <!--FOOTER START -->
-    <footer>
+    <!-- FOOTER START -->
+    <!-- <footer>
         <div class="row">
             <div class="col-md-6 ms-2">
                 <p>Copyright &copy; Kristiin Tribbeck </p>
             </div>
         </div>
-    </footer>
+    </footer> -->
+    <?php
+    include_once('footer.php');
+    ?>
     <!--FOOTER END -->
 </body>
 
