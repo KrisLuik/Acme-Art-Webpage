1306df5c1e98f76019f39ab093766240c8ce5d96
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-11-14 09:29:45 +0800

    Add form validation

--- artist-crud.php
+++ artist-crud.php
@@ -104,19 +104,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
 			        <div class="form-group">
                         <label for="InputArtist">Artist Name</label>
-                        <input type="text" class="form-control" name="InputArtist" placeholder="Enter the artist's name:">
+                        <input type="text" class="form-control<?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" name="InputArtist" placeholder="Enter the artist's name:">
                         <span class="invalid-feedback"><?php echo $name_err;?></span>
                         <br>
                     </div>
                     <div class="form-group">
                         <label for="InputLifeSpan">Lifespan</label>
-                        <input type="text" class="form-control" name="InputLifeSpan" placeholder="Enter the artist lifespan:">
+                        <input type="text" class="form-control<?php echo (!empty($lifespan_err)) ? 'is-invalid' : ''; ?>" name="InputLifeSpan" placeholder="Enter the artist lifespan:">
                         <span class="invalid-feedback"><?php echo $lifespan_err;?></span>
                         <br>
                     </div>
                     <div class="form-group">
                         <label for="InputPortrait">Portrait</label>
-                        <input type="file" class="form-control"name="InputPortrait" placeholder="Portrait of the artist:">
+                        <input type="file" class="form-control<?php echo (!empty($portrait_err)) ? 'is-invalid' : ''; ?>"name="InputPortrait" placeholder="Portrait of the artist:">
                         <span class="invalid-feedback"><?php echo $portrait_err;?></span>
                         <br>
                     </div>
